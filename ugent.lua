29b3083b9dd09e61a8f13ee07624a62f4f860edd
Russell Haley <russ.haley@gmail.com>
2016-02-03 14:09:51 -0800

    updated. Starting to figure this out now.

--- ugent.lua
+++ ugent.lua
@@ -19,13 +19,18 @@ function XML()
 	-- load XML data from file "test.xml" into local table xfile 
 	local xfile = xml.load("comp-parts.xml") 
 	-- search for substatement having the tag "scene" 
-	local xscene = xfile:find("parts") 
+	local xscene = xfile:find("part") 
 	-- if this substatement is found... 
 	if xscene ~= nil then 
 	  -- ...print it to screen
 	  print(xscene)
 	  -- print attribute id and first substatement
-	  print( xscene.id, xscene[1] )
+	  
+	  print("mytag: ", xscene:tag())
+	  print("yourtag: ", xscene[2]:tag())
+	  print("yourvalue: ", xscene[2]:text())
+	  
+	  print( xscene[1].icode, xscene[2])
 	  -- set attribute id
 	  xscene["id"] = "newId"
 	end 
